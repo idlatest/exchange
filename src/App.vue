6c923c9ed2985efd7feea65e2476600d24074ec3
Idongesit Ntuk <ideeofficial@gmail.com>
2019-04-04 20:12:30 +0100

    Add trade history component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen text-grey text-sm bg-black antialiased">
+  <div class="min-h-screen font-sans text-grey text-xs bg-black antialiased">
     <header class="h-16 flex justify-between items-center bg-grey-darkest px-16 flex flex-wrap">
        <div class="flex items-center flex-no-shrink text-white mr-6">
         <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
--- src/pages/Exchange.vue
+++ src/pages/Exchange.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="min-h-10 lg:h-10 lg:flex hidden bg-black shadow-md px-16 items-center">
       <ul class="list-reset block lg:flex">
-        <li class="lg:pr-8 border-r-2">Last Price: <span class="text-white font-semibold text-lg">1,039.99</span></li>
-        <li class="lg:px-8 border-r-2">24h Spread: <span class="text-white font-semibold text-lg">1,039.99</span></li>
-        <li class="lg:px-8 border-r-2">24h High: <span class="text-white font-semibold text-lg">1,039.99</span></li>
-        <li class="lg:px-8 border-r-2">24h Low: <span class="text-white font-semibold text-lg">1,039.99</span></li>
-        <li class="lg:px-8">24h Volume: <span class="text-white font-semibold text-lg">1,039.99</span></li>
+        <li class="lg:pr-8 border-r-2">Last Price: <span class="text-white font-semibold text-base">1,039.99</span></li>
+        <li class="lg:px-8 border-r-2">24h Spread: <span class="text-white font-semibold text-base">1,039.99</span></li>
+        <li class="lg:px-8 border-r-2">24h High: <span class="text-white font-semibold text-base">1,039.99</span></li>
+        <li class="lg:px-8 border-r-2">24h Low: <span class="text-white font-semibold text-base">1,039.99</span></li>
+        <li class="lg:px-8">24h Volume: <span class="text-white font-semibold text-base">1,039.99</span></li>
       </ul>
     </div>
     <div class="flex flex-wrap mx-6 mt-6">
-      <div class="w-full md:w-1/2 lg:w-1/4 md:px-2 mb-6">
+      <div class="w-full md:w-1/2 lg:w-1/4 md:px-2 mb-4">
         <div class="bg-grey-darkest rounded h-90">
           <div class="p-4">
             <h3 class="text-white mb-4">Markets: <span class="ml-6">BLN/BTC</span></h3>
@@ -95,14 +95,14 @@
           </div>
         </div>
       </div>
-      <div class="w-full lg:w-1/2 md:px-2 mb-6">
+      <div class="w-full lg:w-1/2 md:px-2 mb-4">
         <div class="bg-grey-darkest rounded h-90">
           <div class="p-4">
             <h3 class="text-white">Chart</h3>
           </div>
         </div>
       </div>
-      <div class="w-full md:w-1/2 lg:w-1/4 md:px-2 mb-6">
+      <div class="w-full md:w-1/2 lg:w-1/4 md:px-2 mb-4">
         <div class="bg-grey-darkest rounded h-90">
           <div class="p-4">
             <ul class="list-reset flex mb-10 mt-4">
@@ -130,6 +130,313 @@
                 <input type="submit" class="px-4 py-2 rounded bg-green text-white">
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-wrap mx-6">
+      <div class="w-full md:w-1/2 lg:w-1/4 md:px-2 mb-6">
+        <div class="bg-grey-darkest rounded h-90">
+          <div class="p-4">
+            <h3 class="text-white mb-4">Trade History</h3>
+            <div data-simplebar class="overflow-y-auto h-75">
+              <table class="w-full">
+                <thead>
+                  <tr class="mb-6">
+                    <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Price (BTC)</th>
+                    <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Qty (BLN)</th>
+                    <th class="w-1/3 py-3 px-4 text-left">Time</th>
+                  </tr>
+                </thead>
+                <tbody class="">
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-green">&blacktriangle;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-red">&blacktriangledown;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-red">&blacktriangledown;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-red">&blacktriangledown;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-green">&blacktriangle;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-red">&blacktriangledown;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-green">&blacktriangle;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-red">&blacktriangledown;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-green">&blacktriangle;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-red">&blacktriangledown;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                  <tr class="hover:bg-grey-darker">
+                    <td class="w-1/3 py-2 px-4 rounded-l-lg text-white whitespace-no-wrap">
+                      <span class="text-green">&blacktriangle;</span> 550,000.00
+                    </td>
+                    <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                    <td class="w-1/3 py-2 px-4 rounded-r-lg"> 19:55:01</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/2 md:px-2 mb-6">
+        <div class="bg-grey-darkest rounded h-90">
+          <div class="p-4">
+            <div class="flex">
+              <div class="w-1/2 min-h-full pr-4 border-r-2 border-black">
+                <div class="flex justify-between mb-4">
+                  <h3 class="text-white">Buy Orders</h3>
+                  <div class="font-bold">Total: <span class="text-white">550,000.0</span> BTC</div>
+                </div>
+                <div data-simplebar class="w-full overflow-y-auto h-75">
+                  <table class="w-full">
+                    <thead>
+                      <tr class="mb-6">
+                        <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Price (BTC)</th>
+                        <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Qty (BLN)</th>
+                        <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Total (BTC)</th>
+                      </tr>
+                    </thead>
+                    <tbody class="">
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-green">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="w-1/2 pl-4">
+                <div class="flex justify-between mb-4">
+                  <h3 class="text-white">Sell Orders</h3>
+                  <div class="font-bold">Total: <span class="text-white">550,000.0</span> BLN</div>
+                </div>
+                <div data-simplebar class="w-full overflow-y-auto h-75">
+                  <table class="w-full">
+                    <thead>
+                      <tr class="mb-6">
+                        <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Price (BLN)</th>
+                        <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Qty (BTC)</th>
+                        <th class="w-1/3 py-3 px-4 text-left whitespace-no-wrap">Total (BLN)</th>
+                      </tr>
+                    </thead>
+                    <tbody class="">
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                      <tr class="hover:bg-grey-darker">
+                        <td class="w-1/3 py-2 px-4 rounded-l-lg text-white text-red">
+                          550,000.00
+                        </td>
+                        <td class="w-1/3 py-2 px-4 text-white">8000.00</td>
+                        <td class="w-1/3 py-2 px-4 rounded-r-lg text-white"> 10,000.00</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full md:w-1/2 lg:w-1/4 md:px-2 mb-6">
+        <div class="bg-grey-darkest rounded h-90">
+          <div class="p-4">
+            <h3 class="text-white mb-4">My Orders</h3>
+            
           </div>
         </div>
       </div>
