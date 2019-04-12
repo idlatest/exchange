a72a77fa3fb0477958ff23cf6c63a63e74dcb84a
Idongesit Ntuk <ideeofficial@gmail.com>
2019-04-12 19:17:29 +0100

    Make account page mobile responsive

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="mx-6 mt-6 pb-6">
     <h1 class="mb-4">Account</h1>
-    <div class="border-t border-grey-dark px-4 pb-4 pt-6">
-      <div class="flex">
-        <div class="w-1/3 mr-6">
+    <div class="border-t border-grey-dark pb-4 pt-6">
+      <div class="md:flex">
+        <div class="w-full md:w-1/3 mr-6 mb-4">
           <h3 class="mb-4">Profile</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, fugit delectus tempore aspernatur consequuntur tenetur reprehenderit nemo repellendus recusandae adipisci, ratione deleniti.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, fugit delectus tempore aspernatur consequuntur tenetur.</p>
         </div>
-        <div class="w-2/3">
+        <div class="w-full md:w-2/3">
           <div class="bg-grey-darkest p-4 rounded">
-            <div class="flex mb-6">
-              <div class="w-1/2 mr-2">
+            <div class="md:flex">
+              <div class="w-full md:w-1/2 md:mr-2 mb-6">
                 <label for="firstName" class="block mb-2 font-bold">First Name</label>
                 <input type="text" id="firstName" placeholder="Idongesit" class="w-full px-4 py-2 rounded bg-black text-grey">
               </div>
-              <div class="w-1/2 ml-2">
+              <div class="w-full md:w-1/2 md:ml-2 mb-6">
                 <label for="lastName" class="block mb-2 font-bold">Last Name</label>
                 <input type="text" id="lastName" placeholder="Ntuk" class="w-full px-4 py-2 rounded bg-black text-grey">
               </div>
             </div>
-            <div class="flex mb-6">
-              <div class="w-1/2 mr-2">
+            <div class="md:flex">
+              <div class="w-full md:w-1/2 md:mr-2 mb-6">
                 <label for="email" class="block mb-2 font-bold">Email</label>
-                <input type="text" id="email" placeholder="ideeofficial@gmail.com" class="w-full px-4 py-2 rounded bg-black text-grey">
+                <input type="email" id="email" placeholder="ideeofficial@gmail.com" class="w-full px-4 py-2 rounded bg-black text-grey">
               </div>
             </div>
             <div class="text-right">
@@ -32,26 +32,26 @@
         </div>
       </div>
     </div>
-    <div class="border-t border-grey-dark px-4 pb-4 pt-6">
-      <div class="flex">
-        <div class="w-1/3 mr-6">
+    <div class="border-t border-grey-dark pb-4 pt-6">
+      <div class="md:flex">
+        <div class="w-full md:w-1/3 mr-6 mb-4">
           <h3 class="mb-4">Security</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, fugit delectus tempore aspernatur consequuntur tenetur reprehenderit nemo repellendus recusandae adipisci, ratione deleniti.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, fugit delectus tempore aspernatur consequuntur tenetur.</p>
         </div>
-        <div class="w-2/3">
+        <div class="w-full md:w-2/3">
           <div class="bg-grey-darkest p-4 rounded">
-            <div class="flex mb-6">
-              <div class="w-1/2 mr-2">
+            <div class="md:flex">
+              <div class="w-full md:w-1/2 md:mr-2 mb-6">
                 <label for="oldPassword" class="block mb-2 font-bold">Old Password</label>
                 <input type="password" id="oldPassword" class="w-full px-4 py-2 rounded bg-black text-grey">
               </div>
-              <div class="w-1/2 ml-2">
+              <div class="w-full md:w-1/2 md:ml-2 mb-6">
                 <label for="newPassword" class="block mb-2 font-bold">New Password</label>
                 <input type="password" id="newPassword" class="w-full px-4 py-2 rounded bg-black text-grey">
               </div>
             </div>
-            <div class="flex mb-6">
-              <div class="w-1/2 mr-2">
+            <div class="md:flex">
+              <div class="w-full md:w-1/2 md:mr-2 mb-6">
                 <label for="confirmPassword" class="block mb-2 font-bold">Confirm Password</label>
                 <input type="password" id="confirmPassword" class="w-full px-4 py-2 rounded bg-black text-grey">
               </div>
@@ -63,16 +63,16 @@
         </div>
       </div>
     </div>
-    <div class="border-t border-grey-dark px-4 pb-4 pt-6">
-      <div class="flex">
-        <div class="w-1/3 mr-6">
+    <div class="border-t border-grey-dark pb-4 pt-6">
+      <div class="md:flex">
+        <div class="w-full md:w-1/3 mr-6 mb-4">
           <h3 class="mb-4">Preference</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, fugit delectus tempore aspernatur consequuntur tenetur reprehenderit nemo repellendus recusandae adipisci, ratione deleniti.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, fugit delectus tempore aspernatur consequuntur tenetur.</p>
         </div>
-        <div class="w-2/3">
+        <div class="w-full md:w-2/3">
           <div class="bg-grey-darkest p-4 rounded">
-            <div class="flex mb-6">
-              <div class="w-1/3 mr-2">
+            <div class="md:flex">
+              <div class="w-full md:w-1/3 md:ml-2 mb-6">
                 <label for="oldPassword" class="block mb-2 font-bold">Two-factor Authentication</label>
                 <div class="onoffswitch">
                   <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="twofactor" checked>
@@ -82,7 +82,7 @@
                   </label>
                 </div>
               </div>
-              <div class="w-1/3 ml-2">
+              <div class="w-full md:w-1/3 md:mr-2 mb-6">
                 <label for="newPassword" class="block mb-2 font-bold">Trade Fee Discount</label>
                 <div class="onoffswitch">
                   <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="tradfee" checked>
